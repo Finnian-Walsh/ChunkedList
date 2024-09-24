62bf1176291f172add92576be76c6028036ad1a1
Silver <silversupergamer@gmail.com>
2024-09-24 19:32:02 +0100

    Add Result class, macros, Front and back test

--- Test/ChunkedListTest.cpp
+++ Test/ChunkedListTest.cpp
@@ -1,125 +1,161 @@
 
 #include "ChunkedList.hpp"
 
-#include <string>
-#include <cstdlib>
+#include <iostream>
+#include <functional>
 
-template<typename FunctionType, typename OutputType, typename... InputTypes>
-void callFunction(const char functionName[], FunctionType functionPtr, const OutputType& expectedOutput, InputTypes&&... input) {
-  if (expectedOutput != functionPtr(input...)) {
-    throw std::runtime_error(std::string{"Function call produced invalid output:\n"} + functionName);
-  }
+template<size_t ChunkSize = 32, typename T = int, bool ShouldCopy = false>
+using StandardChunkedList = ChunkedList<T, ChunkSize, ShouldCopy>;
+
+#define BEGIN int testNumber{1}; std::cout << "Starting tests..." << std::endl
+#define CALL_TEST(functionName, functionPtr) callFunction(functionName, functionPtr); std::cout << "Test " << testNumber << " successful." << std::endl; ++testNumber;
+#define SUCCESS std::cout << "All " << testNumber << " tests have been ran."; return EXIT_SUCCESS
+#define RETURN_IF(condition, str) if (condition) return Result{std::string{str}};
+
+class Result {
+  public:
+    const std::string message{};
+    const bool status{};
+    
+    explicit Result(bool status) : status{status} {}
+    
+    explicit Result(std::string &&message) : message{std::move(message)} {}
+    
+    explicit Result(const std::string &message) : message{message} {}
+};
+
+void callFunction(const char functionName[], Result(*functionPtr)()) {
+  Result &&result = functionPtr();
+  if (!result.status)
+    throw std::runtime_error(
+    std::string{"Function call failed:\n"}.operator+=(functionName).operator+=('\n').operator+=(result.message));
 }
 
 int main() {
-  callFunction("Iterators", []() -> int {
-    ChunkedList<int> chunkedList{1, 2, 3, 4, 5};
+  BEGIN;
+  
+  CALL_TEST("Front and back", ([]() -> Result {
+    using List = StandardChunkedList<2>;
+    List chunkedList{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    int chunkIndex{};
+    
+    for (auto chunkIt = chunkedList.beginChunk(); chunkIt != chunkedList.endChunk(); ++chunkIt, ++chunkIndex) {
+      int item = chunkIt->operator[](chunkedList.chunk_size() - 1);
+      
+      RETURN_IF(item % 2, "")
+      
+      if (item % 2)
+        return Result{std::string{"Unexpected item found: "}.operator+=(item).operator+=(" at chunk ").operator+=(chunkIndex + '0')};
+    }
+    
+    RETURN_IF((*--chunkedList.endChunk())[chunkedList.chunk_size() - 1] != 10, "Last item is not 10")
+    
+    return Result{true};
+  }))
+  
+  CALL_TEST("Iterators", ([]() -> Result {
+    std::cout << "creating list\n";
+    StandardChunkedList chunkedList{1, 2, 3, 4, 5};
     int total{};
-
-    for (int num : chunkedList)
+    
+    std::cout << "iterating...\n";
+    for (int num: chunkedList)
       total += num;
-
-    return total;
-  }, 15);
-
-  callFunction("Pushing and popping", []() -> char {
-    ChunkedList<char, 4, false> chunkedList{};
+    
+    std::cout << "finished iterating...\n";
+    return Result{total == 15};
+  }))
+  
+  CALL_TEST("Pushing and popping", ([]() -> Result {
+    StandardChunkedList<4, char> chunkedList{};
     
     for (int i = 0; i < 10; ++i) {
       chunkedList.push('a');
       chunkedList.push('b');
     }
-
-    for (int i = 0; i < 19; ++i)
+    
+    for (int i = 20; i > 1; ++i) {
+      RETURN_IF(i != chunkedList.size(), "Unexpected ChunkedList size")
+      
       chunkedList.pop();
-
-    return chunkedList[0];
-  }, 'a');
-
-  callFunction("Equality and inequality", []() -> bool {
-    ChunkedList<int> list1{}, list2{};
-
+    }
+    
+    return chunkedList[0] == 'a' ? Result{true} : Result{std::string{"First item is not 'a'"}};
+  }))
+  
+  CALL_TEST("Equality and inequality", ([]() -> Result {
+    StandardChunkedList list1{}, list2{};
+    
     list1.push(1);
     list1.push(2);
     list1.push(3);
     list1.push(4);
-
+    
     list2.push(1);
     list2.push(2);
     list2.push(3);
     list2.push(3);
-
-    if (list1 != list2)
-      return false;
+    
+    RETURN_IF(list1 != list2, "List comparison failed")
     
     list2.pop();
     list2.push(4);
-
-    if (list1 == list2)
-      return false;
+    
+    RETURN_IF(list1 == list2, "List comparison failed")
     
     for (int i = 0; i < 80; ++i) {
       list1.push(i);
       list2.push(i);
     }
-
+    
     list1.push(1);
     list2.push(1);
-
-    if (list1 != list2)
-      return false;
+    
+    RETURN_IF(list1 != list2, "List comparison failed")
     
     list2.pop();
     list2.push(2);
-
-    if (list1 != list2)
-      return false;
-
+    
+    RETURN_IF(list1 != list2, "List comparison failed")
+    
     auto it1 = list1.begin(), &&it2 = list1.begin();
-
+    
     ++it2;
-
-    if (it1 == it2)
-      return false;
-
+    
+    RETURN_IF(it1 == it2, "Iterator comparison failed")
+    
     --it2;
-
-    if (it1 != it2)
-      return false;
-
+    
+    RETURN_IF(it1 != it2, "Iterator comparison failed")
+    
     it1 += 50;
-
+    
     it2 += 40;
-
+    
     for (int i = 0; i < 10; ++i)
       ++it2;
-
-    if (it1 != it2)
-      return false;
-
-    return it1 == it2;
-  }, bool{true});
-
-  callFunction("Concatenation and indexing", []() -> bool {
-    ChunkedList<int, 2> chunkedList;
-
+    
+    return it1 == it2 ? Result{true} : Result{std::string{"Iterator comparison failed"}};
+  }))
+  
+  CALL_TEST("Concatenation and indexing", ([]() -> Result {
+    StandardChunkedList<2> chunkedList;
+    
     for (int i{}; i < 10; ++i)
       chunkedList.push(i);
-
-    if (std::string{chunkedList.concat(" ")} != std::string{"1 2 3 4 5 6 7 8 9 10"})
-      return false;
-
+    
+    RETURN_IF(std::string{"1 2 3 4 5 6 7 8 9 10"} != chunkedList.concat(" "), "Concatenation failed")
+    
     for (int i{}; i < 10; ++i)
-      if (chunkedList[i] != i)
-        return false;
-
-    return true;
-
-  }, bool{true});
-
+      RETURN_IF(chunkedList[i] != i, "")
+    
+    return Result{true};
+  }))
+  
   // callFunction("", []() -> bool {
   //   return true;
   // }, bool{true});
-
-  return 0;
+  
+  SUCCESS;
 }
