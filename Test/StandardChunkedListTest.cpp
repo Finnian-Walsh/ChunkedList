2dd8a6393b02874b3efa2c97d173404eee59b50b
Silver <silversupergamer@gmail.com>
2024-10-21 23:30:28 +0100

    Add list creation test and CHUNKED_LIST_DEBUGGING definition

--- Test/StandardChunkedListTest.cpp
+++ Test/StandardChunkedListTest.cpp
@@ -1,10 +1,24 @@
 
-#include "core/TestUtility.hpp"
+
+#define CHUNKED_LIST_DEBUGGING
 #include "StandardChunkedList.hpp"
+
+#include "core/TestUtility.hpp"
 
 template<size_t ChunkSize = 32, typename T = int, bool ShouldCopy = false>
 using TestChunkedList = StandardChunkedList<T, ChunkSize, ShouldCopy>;
 
 int main() {
-  TestChunkedList<> testList{};
+  BEGIN
+  
+  CALL_TEST("List creation", []() -> Result {
+    potentialError = "List creation";
+    std::cout << "Creating list";
+    TestChunkedList<> testList{};
+    std::cout << "\nCreated";
+    
+    return Result{true};
+  })
+  
+  SUCCESS
 }
