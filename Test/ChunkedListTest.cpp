7414264eeec26800e4e0cc04241d4f9b29b9cf3f
Finnian-Walsh <finnian.t.walsh@gmail.com>
2025-01-02 22:10:17 +0000

    Create Tests namespace for test generic tests, implement multiple tests per test call

--- Test/ChunkedListTest.cpp
+++ Test/ChunkedListTest.cpp
@@ -1,226 +1,302 @@
-#define CHUNKED_LIST_DEBUGGING
+// #define CHUNKED_LIST_DEBUGGING
 
 #include "ChunkedList.hpp"
 
-#define LOG_LEVEL 2
+#define LOG_LEVEL 0
+
 #include "core/TestUtility.hpp"
 
-template<size_t ChunkSize = 32, typename T = int, bool ShouldCopy = false>
+template<size_t ChunkSize = 32, typename T = int, bool ShouldCopy = true>
 using TestChunkedList = ChunkedList<T, ChunkSize, ShouldCopy>;
 
 using namespace TestUtility;
 
+namespace Tests {
+  template<size_t ChunkSize>
+  void FrontAndBack() {
+    using List = TestChunkedList<ChunkSize>;
+    performTask("Creating chunked list with initializer list");
+    List chunkedList{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    auto front = reinterpret_cast<ChunkedListAccessor<int, ChunkSize, true>*>(&chunkedList)->getFront();
+    auto back = reinterpret_cast<ChunkedListAccessor<int, ChunkSize, true>*>(&chunkedList)->getBack();
+
+    THROW_IF((*front)[0] != 1, "First item is not 1")
+    THROW_IF((*back)[back->nextIndex - 1] != 10, "Last item is not 10")
+
+    performTask("Calling size()");
+    const size_t size = chunkedList.size();
+
+    THROW_IF(size != 10, std::string{"Unexpected size; got "}+=std::to_string(size)+=" but expected 10");
+
+    performTask("Indexing");
+    THROW_IF(chunkedList[size - 1] != 10, "Last item is not 10")
+  }
+
+  template<size_t ChunkSize>
+  void ConcatenationIndex() {
+    TestChunkedList<ChunkSize> chunkedList;
+
+    performTask("Pushing");
+    for (int i = 0; i < 10; ++i)
+      chunkedList.push(i); {
+      performTask("Concatenation");
+      std::string &&str = chunkedList.concat(" ");
+      THROW_IF(str != "0 1 2 3 4 5 6 7 8 9",
+               (std::string{"Concatenation failed; expected \"0 1 2 3 4 5 6 7 8 9\" but got \""}+=str)+='"')
+    }
+
+    performTask("Indexing");
+    for (int i = 0; i < 10; ++i) {
+      const int num = chunkedList[i];
+      THROW_IF(num != i,
+               (std::string{"Index "}+=std::to_string(i)+=" was expected to be ")+=std::to_string(i)+=std::string{
+               " but received "}+=std::to_string(num));
+    }
+  }
+
+  template<size_t ChunkSize>
+  void EqualityInequality() {
+    TestChunkedList<ChunkSize> list1{};
+    TestChunkedList<ChunkSize> list2{};
+
+    performTask("Pushing");
+
+    for (int i = 1; i < 4; ++i) {
+      list1.push(i);
+      list2.push(i);
+    }
+
+    list1.push(4);
+    list2.push(3);
+
+    THROW_IF(list1 == list2, "List comparison 1 failed")
+
+    performTask("Popping");
+    list2.pop();
+
+    performTask("Pushing");
+    list2.push(4);
+
+    performTask("ChunkedList inequality");
+    THROW_IF(list1 != list2, "List comparison 2 failed")
+
+    performTask("Pushing");
+    for (int i = 0; i < 80; ++i) {
+      list1.push(i);
+      list2.push(i);
+    }
+
+    list1.push(1);
+    list2.push(1);
+
+    performTask("ChunkedList inequality");
+    THROW_IF(list1 != list2, "List comparison 3 failed")
+
+    performTask("Popping");
+    list2.pop();
+
+    performTask("Pushing");
+    list2.push(2);
+
+    performTask("List equality");
+    THROW_IF(list1 == list2, "List comparison 4 failed")
+  }
+
+  template<SortType SortingAlgorithm>
+  void Sorting() {
+    performTask("List creation");
+    TestChunkedList<> list{};
+
+    performTask("Random Number Generator creation");
+    RandomNumberGenerator rng;
+
+    performTask("Pushing or using the RNG");
+    for (int i{}; i < 100; ++i) {
+      list.push(rng(1, 100));
+    }
+
+    performTask("Sorting");
+    list.sort<std::less<int>, SortingAlgorithm>();
+
+    performTask("Indexing");
+    int last = list[0];
+
+    performTask("Calling begin()");
+    auto iterator = list.begin();
+
+    performTask("Iterator operator+=()");
+    iterator += 1;
+
+    performTask("Calling end()");
+    const auto end = list.end();
+
+    performTask("Iterator inequality");
+    for (; iterator != end; ++iterator) {
+      performTask("Iterator dereferencing");
+      THROW_IF(*iterator < last, "Sorting failed!")
+
+      last = *iterator;
+
+      performTask("Iterator operator++(int)");
+    }
+  }
+
+  template<size_t ChunkSize>
+  void Insertion() {
+    TestChunkedList<ChunkSize> chunkedList{5, 10, 15};
+    std::string expectedOutput{"[5, 10, 15"};
+
+    RandomNumberGenerator rng;
+
+    for (int i = 0; i < 1000; ++i) {
+      int num = rng(-1000, 1000);
+      expectedOutput += ", ";
+      expectedOutput += std::to_string(num);
+      chunkedList.push(num);
+    }
+
+    expectedOutput.push_back(']');
+
+    performTask("Inserting ChunkedList into ostream");
+    std::ostringstream os{};
+    os << chunkedList;
+
+    THROW_IF(os.str() != expectedOutput,
+             std::string("ostream insertion ran incorrectly\nGot: ") += os.str())
+  }
+}
+
+
 int main() {
   BEGIN
 
-  CALL_TEST("Front and back", ([]() -> Result {
-              performOperation("unknown", 2);
-              constexpr size_t chunkSize = 2;
-              using List = TestChunkedList<chunkSize>;
-              performOperation("Creating chunked list with initializer list");
-              List chunkedList{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  using namespace Tests;
 
-              int chunkIndex{};
+  callFunction("Front and back", [] {
+    testData.setSource("Test 1");
+    FrontAndBack<1>();
 
-              performOperation("Calling beginChunk()");
-              auto chunkIt = chunkedList.beginChunk();
+    testData.setSource("Test 2");
+    FrontAndBack<2>();
 
-              performOperation("calling endChunk()");
-              const auto endChunk = chunkedList.endChunk();
+    testData.setSource("Test 3");
+    FrontAndBack<3>();
 
-              performOperation("ChunkIterator inequality");
-              for (; chunkIt != endChunk; ++chunkIt, ++chunkIndex) {
-              performOperation("Dereferencing ChunkIterator or Chunk indexing");
-              const int item = chunkIt->operator[](chunkSize - 1);
+    testData.setSource("Test 4");
+    FrontAndBack<4>();
 
-              performOperation("String concatenation");
-              RETURN_IF(item & 1,
-                std::string{"Unexpected item found: "}.operator+=(item).operator+=(" at chunk ").operator+=(
-                  chunkIndex + '0'))
+    testData.setSource("Test 5");
+    FrontAndBack<5>();
+  });
 
-              performOperation("ChunkIterator operator++(int)");
-              }
+  callFunction("Insertion", [] {
+    testData.setSource("Test 1");
+    Insertion<1>();
 
-              performOperation("Indexing");
-              RETURN_IF(chunkedList[chunkedList.size() - 1] != 10, "Last item is not 10")
+    testData.setSource("Test 2");
+    Insertion<2>();
 
-              performOperation("Returning");
-              return Result{true};
-              }))
+    testData.setSource("Test 3");
+    Insertion<3>();
 
-  CALL_TEST("Insertion", ([]() -> Result {
-              TestChunkedList<> chunkedList{5, 10, 15};
-              std::string expectedOutput{"[5, 10, 15"};
+    testData.setSource("Test 4");
+    Insertion<4>();
 
-              RandomNumberGenerator rng;
+    testData.setSource("Test 5");
+    Insertion<5>();
+  });
 
-              for (int i = 0; i < 1000; ++i) {
-              int num = rng(-1000, 1000);
-              expectedOutput += ", ";
-              expectedOutput += std::to_string(num);
-              chunkedList.push(num);
-              }
+  callFunction("Sorting", [] {
+    testData.setSource("Bubble Sorting");
+    Sorting<BubbleSort>();
 
-              expectedOutput.push_back(']');
+    testData.setSource("Selection Sorting");
+    Sorting<SelectionSort>();
 
-              potentialError = "Inserting ChunkedList into ostream";
-              std::ostringstream os{};
-              os << chunkedList;
+    testData.setSource("Insertion Sorting");
+    Sorting<InsertionSort>();
 
-              RETURN_IF(os.str() != expectedOutput,
-                std::string("ostream insertion ran incorrectly\nGot: ") += (os.str()))
+    testData.setSource("Merge Sorting");
+    Sorting<MergeSort>();
 
-              return Result{true};
-              }))
+    testData.setSource("Quick Sorting");
+    Sorting<QuickSort>();
 
-  CALL_TEST("Sorting", ([]() -> Result {
-              int num{};
+    testData.setSource("Heap Sorting");
+    Sorting<HeapSort>();
+  });
 
-              potentialError = "List creation";
-              TestChunkedList<> list{};
+  callFunction("Iterators", [] {
+    performTask("List creation");
+    TestChunkedList<> chunkedList{1, 2, 3, 4, 5};
+    int total{};
 
-              potentialError = "Random Number Generator creation";
-              RandomNumberGenerator rng;
+    performTask("List iteration (implicit)");
+    for (const int num: chunkedList)
+      total += num;
 
-              potentialError = "Pushing or using the RNG";
-              for (int i{}; i < 100; ++i) {
-              list.push(rng(1, 100));
-              }
+    THROW_IF(total != 15, "List sum is not 15")
+  });
 
-              potentialError = "Sorting";
-              list.sort();
+  callFunction("Pushing and popping", [] {
+    TestChunkedList<4, char> chunkedList{};
 
-              potentialError = "Indexing";
-              int last = list[0];
+    performTask("Pushing");
+    for (int i = 0; i < 10; ++i) {
+      chunkedList.push('a');
+      chunkedList.push('b');
+    }
 
-              potentialError = "calling begin()";
-              auto iterator = list.begin();
+    for (int i = 20; i > 1; --i) {
+      performTask("Calling size()");
+      const size_t size = chunkedList.size();
+      THROW_IF(i != size,
+               ((std::string{"Unexpected ChunkedList size; expected "}+=std::to_string(i))+=" but got ")+=std::
+               to_string(size));
 
-              potentialError = "Iterator operator+=()";
-              iterator += 1;
+      performTask("Popping");
+      chunkedList.pop();
+    }
 
-              potentialError = "calling end()";
-              auto end = list.end();
+    performTask("Indexing");
+    const char firstItem = chunkedList[0];
+    THROW_IF(firstItem != 'a', std::string{"First item is not 'a'; got "}+=firstItem);
+  });
 
-              potentialError = "Iterator inequality";
-              for (; iterator != end; ++iterator) {
-              potentialError = "Iterator dereferencing";
-              if (*iterator < last) {
-              return Result{"Sorting failed!"};
-              }
+  callFunction("Equality and inequality", [] {
+    testData.setSource("Test 1");
+    EqualityInequality<1>();
 
-              last = *iterator;
+    testData.setSource("Test 2");
+    EqualityInequality<2>();
 
-              potentialError = "Iterator operator++(int)";
-              }
+    testData.setSource("Test 3");
+    EqualityInequality<3>();
 
-              return Result{true};
-              }))
+    testData.setSource("Test 4");
+    EqualityInequality<4>();
 
-  CALL_TEST("Iterators", ([]() -> Result {
-              potentialError = "List creation";
-              TestChunkedList<> chunkedList{1, 2, 3, 4, 5};
-              int total{};
+    testData.setSource("Test 5");
+    EqualityInequality<5>();
+  });
 
-              potentialError = "List iteration (implicit)";
-              for (int num: chunkedList)
-              total += num;
+  callFunction("Concatenation and indexing", [] {
+    testData.setSource("Test 1");
+    ConcatenationIndex<1>();
 
-              return Result{total == 15};
-              }))
+    testData.setSource("Test 2");
+    ConcatenationIndex<2>();
 
-  CALL_TEST("Pushing and popping", ([]() -> Result {
-              TestChunkedList<4, char> chunkedList{};
+    testData.setSource("Test 3");
+    ConcatenationIndex<3>();
 
-              potentialError = "Pushing";
-              for (int i = 0; i < 10; ++i) {
-              chunkedList.push('a');
-              chunkedList.push('b');
-              }
+    testData.setSource("Test 4");
+    ConcatenationIndex<4>();
 
-              for (int i = 20; i > 1; ++i) {
-              potentialError = "Calling size()";
-              RETURN_IF(i != chunkedList.size(), "Unexpected ChunkedList size")
-
-              potentialError = "Popping";
-              chunkedList.pop();
-              }
-
-              potentialError = "Indexing";
-              return chunkedList[0] == 'a' ? Result{true} : Result{std::string{"First item is not 'a'"}};
-              }))
-
-  CALL_TEST("Equality and inequality", ([]() -> Result {
-              TestChunkedList<> list1{};
-              TestChunkedList<> list2;
-
-              potentialError = "Pushing";
-
-              list1.push(1);
-              list1.push(2);
-              list1.push(3);
-              list1.push(4);
-
-              list2.push(1);
-              list2.push(2);
-              list2.push(3);
-              list2.push(3);
-
-              RETURN_IF(list1 == list2, "List comparison failed")
-
-              potentialError = "Popping";
-              list2.pop();
-
-              potentialError = "Pushing";
-              list2.push(4);
-
-              potentialError = "ChunkedList inequality";
-              RETURN_IF(list1 != list2, "List comparison failed")
-
-              potentialError = "Pushing";
-              for (int i = 0; i < 80; ++i) {
-              list1.push(i);
-              list2.push(i);
-              }
-
-              list1.push(1);
-              list2.push(1);
-
-              potentialError = "ChunkedList inequality";
-              RETURN_IF(list1 != list2, "List comparison failed")
-
-              potentialError = "Popping";
-              list2.pop();
-
-              potentialError = "Pushing";
-              list2.push(2);
-
-              potentialError = "List equality";
-              RETURN_IF(list1 == list2, "List comparison failed")
-
-              return Result{true};
-              }))
-
-  CALL_TEST("Concatenation and indexing", ([]() -> Result {
-              TestChunkedList<2> chunkedList;
-
-              potentialError = "Pushing";
-              for (int i{}; i < 10; ++i)
-              chunkedList.push(i);
-
-              potentialError = "Concatenation";
-              RETURN_IF((chunkedList.concat(std::string{" "})) != "0 1 2 3 4 5 6 7 8 9", "Concatenation failed")
-
-              potentialError = "Indexing";
-              for (int i{}; i < 10; ++i)
-              RETURN_IF(chunkedList[i] != i, "")
-
-              return Result{true};
-              }))
-
-  // callFunction("", []() -> bool {
-  //   return true;
-  // }, bool{true});
+    testData.setSource("Test 5");
+    ConcatenationIndex<5>();
+  });
 
   SUCCESS
 }
