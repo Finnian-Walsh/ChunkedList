ed443c8ac3ad4996a50842e6155f58a2c32e0d57
Finnian-Walsh <finnian.t.walsh@gmail.com>
2025-01-08 16:22:08 +0000

    CustomTests.cpp: Add std::vector and std::list tests

--- Test/test_files/CustomTests.cpp
+++ Test/test_files/CustomTests.cpp
@@ -3,21 +3,63 @@
 #include <chrono>
 #include <ranges>
 #include <numeric>
+#include <vector>
+#include <list>
 
 using namespace TestUtility;
 
 int main() {
+  constexpr size_t pushes = 1'000;
+
   callFunction("Integer push time test", [] {
-    ChunkedList<int, 1'000'000'000> testList;
+    ChunkedList<int, pushes> testList;
 
     std::cout << "starting..." << std::endl;
 
     const auto start = std::chrono::high_resolution_clock::now();
 
-    constexpr size_t pushes = 1'000'000'000;
-
     for (int i = 0; i < pushes; i++) {
       testList.push(i);
+    }
+
+    const auto end = std::chrono::high_resolution_clock::now();
+    const auto duration_s = std::chrono::duration_cast<std::chrono::duration<double> >(end - start);
+    const auto duration_ns = std::chrono::duration_cast<std::chrono::duration<double, std::nano> >(end - start);
+
+    std::cout << "Total time taken to push " << pushes << " integers: " << duration_s.count() << " seconds\n";
+    std::cout << "Average time to push 1 integer: " << duration_ns.count() / pushes << " nanoseconds" << std::endl;
+  });
+
+  callFunction("Integer push time test", [] {
+    std::list<int> testList;
+
+    std::cout << "starting..." << std::endl;
+
+    const auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < pushes; i++) {
+      testList.push_back(i);
+    }
+
+    const auto end = std::chrono::high_resolution_clock::now();
+    const auto duration_s = std::chrono::duration_cast<std::chrono::duration<double> >(end - start);
+    const auto duration_ns = std::chrono::duration_cast<std::chrono::duration<double, std::nano> >(end - start);
+
+    std::cout << "Total time taken to push " << pushes << " integers: " << duration_s.count() << " seconds\n";
+    std::cout << "Average time to push 1 integer: " << duration_ns.count() / pushes << " nanoseconds" << std::endl;
+  });
+
+  callFunction("Integer push time test", [] {
+    std::vector<int> testList;
+
+    std::cout << "starting..." << std::endl;
+
+    testList.reserve(pushes);
+
+    const auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < pushes; i++) {
+      testList.push_back(i);
     }
 
     const auto end = std::chrono::high_resolution_clock::now();
